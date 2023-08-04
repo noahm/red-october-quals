a7654382ade0ef11a7c4873634994f1e7772ed11
Noah Manneschmidt <noah@manneschmidt.net>
2023-08-03 21:33:07 -0700

    Allow setting song index via query param

--- app/api/refetch/route.ts
+++ app/api/refetch/route.ts
@@ -1,21 +1,27 @@
 import { songs } from "@/data/event-data";
 import { refreshCacheForSong } from "@/data/statmaniax";
 import { kv } from "@vercel/kv";
-import { NextResponse } from "next/server";
+import { NextRequest, NextResponse } from "next/server";
 
 const lastIndexKey = "special:api/refetch;last-index";
 
-export async function GET(_: Request) {
-  const lastIndex = await kv.get<number>(lastIndexKey);
-  const refetchIndex = lastIndex === null ? 0 : (lastIndex + 1) % songs.length;
-  const randomSong = songs[refetchIndex];
-  await refreshCacheForSong(randomSong.song);
+export async function GET(req: NextRequest) {
+  const queryIndex = req.nextUrl.searchParams.get("index");
+  let refetchIndex: number;
+  if (queryIndex) {
+    refetchIndex = parseInt(queryIndex, 10) % songs.length;
+  } else {
+    const lastIndex = await kv.get<number>(lastIndexKey);
+    refetchIndex = lastIndex === null ? 0 : (lastIndex + 1) % songs.length;
+  }
+  const songToRefetch = songs[refetchIndex];
+  await refreshCacheForSong(songToRefetch.song);
   await kv.set(lastIndexKey, refetchIndex);
   return NextResponse.json(
     {
       status: "ok",
       index: refetchIndex,
-      song: randomSong.title,
+      song: songToRefetch.title,
     },
     {
       headers: {
